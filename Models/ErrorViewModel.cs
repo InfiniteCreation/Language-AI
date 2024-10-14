55dd2774034f5aa4f830a5ad6ac82978f1ac8e2a
howling <howling.wolves76@gmail.com>
2024-10-14 15:59:55 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Language_AI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
